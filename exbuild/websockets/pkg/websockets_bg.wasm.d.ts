3f02f769cf2018cef69895838610c6021690f662
hamza1311 <muhammadhamza1311@gmail.com>
2023-11-30 17:57:52 +0000

    Deploying to gh-pages from @ rustwasm/wasm-bindgen@cfe3dc28a101a1b4cf7ab7ca6db358eadc7a4063 🚀

---
+++ exbuild/websockets/pkg/websockets_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function start_websocket(): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__h906b36980f600ac8(a: number, b: number, c: number): void;
+export function wasm_bindgen__convert__closures__invoke0_mut__h1acb9f087cebdca7(a: number, b: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
